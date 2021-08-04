ea03aeeba56504f684cdad921e6a7a370dbe05d9
System Administrator <root@Anouars-MacBook-Pro.local>
2021-08-04 17:32:30 +0200

    add modelForm

--- todo/views.py
+++ todo/views.py
@@ -2,26 +2,31 @@ from django.shortcuts import render, redirect
 from django.views.decorators.http import require_POST 
 
 from .models import Todo
-from .forms import TodoForm
+from .forms import TodoForm, NewTodoForm
 
 def index(request):
 	todo_list = Todo.objects.order_by('id')
 
 	form = TodoForm()
+	newtodoform = NewTodoForm()
 
-	context = {'todo_list': todo_list, 'form': form}
+	context = {'todo_list': todo_list, 'form': newtodoform}
 
 	return render(request, 'todo/index.html', context) 
 	
 
 @require_POST	
 def addTodo(request):
-	form = TodoForm(request.POST)
+	#form = TodoForm(request.POST)
 	#print(request.POST['text'])
-
-	if form.is_valid():
-		new_todo = Todo(text=request.POST['text'])
-		new_todo.save()
+	# ----to change a single itedm wene adding a new item ----
+	#todo_31 = Todo.objects.get(pk=31)
+	#newtodoform = NewTodoForm(request.POST, instance=todo_31)
+	newtodoform = NewTodoForm(request.POST)
+	if newtodoform.is_valid():
+		#new_todo = Todo(text=form.cleaned_data['text'])
+		#new_todo.save()
+		new_todo = newtodoform.save()
 
 	return redirect('index')
 
